e194bcc132306a119f0617010bb23dff7aa50f11
CaioPQD <128498808+CaioPQD@users.noreply.github.com>
2023-06-23 23:51:35 -0300

    Add files via upload

---
+++ questão4/questao4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o caminho completo do arquivo de texto: ");
+        string caminhoArquivo = Console.ReadLine();
+
+        try
+        {
+            // Lê todo o conteúdo do arquivo
+            string conteudo = File.ReadAllText(caminhoArquivo);
+
+            // Conta a quantidade de caracteres 'a' no conteúdo
+            int quantidadeA = 0;
+            foreach (char c in conteudo)
+            {
+                if (c == 'a' || c == 'A')
+                {
+                    quantidadeA++;
+                }
+            }
+
+            // Exibe a quantidade de caracteres 'a'
+            Console.WriteLine("A quantidade de caracteres 'a' no arquivo é: " + quantidadeA);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("O arquivo não foi encontrado.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ocorreu um erro ao ler o arquivo: " + ex.Message);
+        }
+    }
+}
---
+++ questão5/questao5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o caminho completo do arquivo de texto: ");
+        string caminhoArquivo = Console.ReadLine();
+
+        try
+        {
+            // Verifica se o arquivo existe
+            if (!File.Exists(caminhoArquivo))
+            {
+                Console.WriteLine("O arquivo não foi encontrado.");
+                return;
+            }
+
+            // Lê todas as linhas do arquivo
+            string[] linhas = File.ReadAllLines(caminhoArquivo);
+
+            // Imprime o conteúdo linha a linha
+            foreach (string linha in linhas)
+            {
+                Console.WriteLine(linha);
+            }
+
+            // Exibe a quantidade de linhas
+            int quantidadeLinhas = linhas.Length;
+            Console.WriteLine($"O arquivo possui {quantidadeLinhas} linha(s).");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ocorreu um erro ao ler o arquivo: " + ex.Message);
+        }
+    }
+}

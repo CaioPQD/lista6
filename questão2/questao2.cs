944480ce06a15d8c954ae96363157b33241bfe28
CaioPQD <128498808+CaioPQD@users.noreply.github.com>
2023-06-23 23:48:20 -0300

    Add files via upload

---
+++ questão2/questao2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma frase:");
+        string frase = Console.ReadLine();
+
+        string fraseSemVogais = RemoverVogais(frase);
+
+        Console.WriteLine("Frase sem vogais: " + fraseSemVogais);
+    }
+
+    static string RemoverVogais(string texto)
+    {
+        string vogais = "aeiouAEIOU";
+        string resultado = "";
+
+        foreach (char c in texto)
+        {
+            if (!vogais.Contains(c))
+            {
+                resultado += c;
+            }
+        }
+
+        return resultado;
+    }
+}
---
+++ questão3/questao3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite uma frase:");
+        string frase = Console.ReadLine();
+
+        string fraseCodificada = CodificarCesar(frase);
+
+        Console.WriteLine("Nova string: " + fraseCodificada);
+    }
+
+    static string CodificarCesar(string texto)
+    {
+        int deslocamento = 3; // número fixo de posições para a troca
+
+        string resultado = "";
+
+        foreach (char c in texto)
+        {
+            if (char.IsLetter(c)) // verifica se o caractere é uma letra
+            {
+                char letra = char.ToUpper(c); // converte a letra para maiúscula
+                int codigoAscii = ((letra - 'A' + deslocamento) % 26) + 'A'; // aplica o deslocamento no código ASCII da letra
+
+                char letraCodificada = (char)codigoAscii; // converte o código ASCII de volta para caractere
+
+                resultado += letraCodificada;
+            }
+            else
+            {
+                resultado += c;
+            }
+        }
+
+        return resultado;
+    }
+}
